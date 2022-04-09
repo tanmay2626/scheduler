ca03695f44a9f339cee0087bf9e85d5a4ac1e02f
Tanmay Waykar <87329832+tanmay2626@users.noreply.github.com>
2022-04-09 14:43:38 +0530

    slack schedule every 10 min

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,37 @@
 import schedule
 import datetime
 import time 
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+#def task():
+#    curr = datetime.datetime.now().strftime("%H:%M:%S")
+#    print("Hello world "+str(curr))
+
+#schedule.every(1).minutes.at(":00").do(task)
+
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
+
+client = WebClient(token="xoxb-3232981397716-3257672269472-SIAZQmW6ZPkie6iPkhDHQTm0")
+logger = logging.getLogger(__name__)
+channel_id = "C036L0W9R46"
 
 def task():
     curr = datetime.datetime.now().strftime("%H:%M:%S")
-    print("Hello world "+str(curr))
+    try:
+        result = client.chat_postMessage(
+        channel=channel_id,
+        text="Hello world "+str(curr)
+    )
+        print(result)
+    
+    except SlackApiError as e:
+        print(f"Error: {e}")
 
-schedule.every(1).minutes.at(":00").do(task)
+schedule.every(10).minutes.at(":00").do(task)
 
 while True:
     schedule.run_pending()
